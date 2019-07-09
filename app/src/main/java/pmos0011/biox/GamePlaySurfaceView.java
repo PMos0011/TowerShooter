fd7ae10625284e66c4a368073b542b73708ca00b
Paweł <pawel.moskwa.pm@gmail.com>
2019-07-09 17:35:50 +0200

    Add empty OpenGLSurface

---
+++ app/src/main/java/pmos0011/biox/GamePlaySurfaceView.java
@@ -0,0 +1,20 @@
+package pmos0011.biox;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class GamePlaySurfaceView extends GLSurfaceView {
+
+    private final GamePlayRenderer renderer;
+
+    public GamePlaySurfaceView(Context context) {
+        super(context);
+
+        setEGLContextClientVersion(3);
+
+        renderer = new GamePlayRenderer();
+
+        setRenderer(renderer);
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+}
